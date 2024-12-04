e49015c8db13be038aca75c0f6fcf488fc1b4dd5
nishijima <nobukinishijima@valuup.jp>
2024-12-04 19:05:40 +0900

    変更内容の説明

--- composables/useSheet.js
+++ composables/useSheet.js
@@ -1,8 +1,8 @@
 let sheetRange = "";
 
 const getVars = () => {
-    const SPREAD_SHEET_ID = "1O-QM0aFezydz2RPQa6s7VVKSDyDNLeAYD1ckjj84L_k";
-    const GOOGLE_API_KEY = "AIzaSyCb0lSLQwbrNesBm3jPfkoIIMdtFLlQh-o";
+    const SPREAD_SHEET_ID = "1oxec1rNOVNT_l0HDZkH5yu6vZL2gVLJ7TaTnkChRfwY";
+    const GOOGLE_API_KEY = "AIzaSyB0_g8XdNTyfC_TApvn2ZTkZIxH8AC7ijk";
 
     return { SPREAD_SHEET_ID, GOOGLE_API_KEY }
 }
@@ -15,76 +15,98 @@ export async function getKamokuRows() {
     return await useFetch(url)
 }
 
-export async function getProjectRows() {
-    sheetRange = "案件!A2:A";
+// データを取得し、整形して返す関数
+export async function getProjectData() {
+    sheetRange = "事業タイプ!A2:G";
     const { SPREAD_SHEET_ID, GOOGLE_API_KEY } = getVars();
-    const url = 
-        `https://sheets.googleapis.com/v4/spreadsheets/${SPREAD_SHEET_ID}/values/${sheetRange}?key=${GOOGLE_API_KEY}`
-    return await useFetch(url)
-}
-
-export async function getEarnKamokuRows() {
-    sheetRange = "売上分類!A2:A";
-    const { SPREAD_SHEET_ID, GOOGLE_API_KEY } = getVars();
-    const url = 
-        `https://sheets.googleapis.com/v4/spreadsheets/${SPREAD_SHEET_ID}/values/${sheetRange}?key=${GOOGLE_API_KEY}`
-    return await useFetch(url)
-}
-
-export async function getPrivateKamokuRows() {
-    sheetRange = "プライベート管理!A2:A";
-    const { SPREAD_SHEET_ID, GOOGLE_API_KEY } = getVars();
-    const url = 
-        `https://sheets.googleapis.com/v4/spreadsheets/${SPREAD_SHEET_ID}/values/${sheetRange}?key=${GOOGLE_API_KEY}`
-    return await useFetch(url)
-}
-
-export async function singleRow(row) {
-    const { SPREAD_SHEET_ID, GOOGLE_API_KEY } = getVars();
-
-    const rowRange = `Sheet1!A${row}:D${row}`
-
-    const url = `https://sheets.googleapis.com/v4/spreadsheets/${SPREAD_SHEET_ID}/values/${rowRange}?key=${GOOGLE_API_KEY}`
-    const response = await useFetch(url)
-    return response.data
-}
-
-export async function analyticsRows() {
-    const { SPREAD_SHEET_ID, GOOGLE_API_KEY } = getVars();
-    
-    const inputEarningRange = "売上入力!A:G";
-    const inputExpensisRange = "経費入力!A:H";
-    
-    const earningUrl = `https://sheets.googleapis.com/v4/spreadsheets/${SPREAD_SHEET_ID}/values/${inputEarningRange}?key=${GOOGLE_API_KEY}`;
-    const expensisUrl = `https://sheets.googleapis.com/v4/spreadsheets/${SPREAD_SHEET_ID}/values/${inputExpensisRange}?key=${GOOGLE_API_KEY}`;
-    
-    try {
-        const [earningResponse, expensisResponse] = await Promise.all([useFetch(earningUrl), useFetch(expensisUrl)]);
-        
-        return {
-            earningData: earningResponse.data,
-            expensesData: expensisResponse.data,
-        };
-    } catch (error) {
-        console.error('Error fetching data:', error);
-        return { error };
+    const url = `https://sheets.googleapis.com/v4/spreadsheets/${SPREAD_SHEET_ID}/values/${sheetRange}?key=${GOOGLE_API_KEY}`;
+    const response = await fetch(url);
+  
+    if (!response.ok) {
+      throw new Error("スプレッドシートのデータ取得に失敗しました。");
     }
-}
+  
+    const data = await response.json();
+  
+    // データを整形
+    const rows = data.values || [];
+    const projectMap = {};
+  
+    rows.forEach(([type, detail, billingType, itemId, account, invoiceTitle,bikou]) => {
+    //   if (!projectMap[type]) {
+    //     projectMap[type] = [];
+    //   }
+    //   projectMap[type].push(detail);
+        if (!projectMap[detail]) {
+            projectMap[detail] = {
+                type,
+                billingType,
+                itemId,
+                account,
+                invoiceTitle,
+                bikou
+            };
+        }
+    });
+    console.log(projectMap);
+  
+    return projectMap;
+  }
+
+  export async function getClientData() {
+
+    sheetRange = "取引先マスタ!A2:E"; // スプレッドシートの範囲
+    const { SPREAD_SHEET_ID, GOOGLE_API_KEY } = getVars();
+    const url = `https://sheets.googleapis.com/v4/spreadsheets/${SPREAD_SHEET_ID}/values/${sheetRange}?key=${GOOGLE_API_KEY}`;
+    const response = await fetch(url);
+  
+    try {
+      if (!response.ok) {
+        throw new Error("スプレッドシートデータの取得に失敗しました。");
+      }
+  
+      const data = await response.json();
+      const rows = data.values;
+  
+      // データ整形
+      const clientMap = {};
+      rows.forEach(([name, id, departmentName, departmentId, businessDetails]) => {
+        if (!businessDetails) return;
+  
+        businessDetails.split(",").forEach((detail) => {
+          const trimmedDetail = detail.trim();
+          if (!clientMap[trimmedDetail]) {
+            clientMap[trimmedDetail] = [];
+          }
+          clientMap[trimmedDetail].push({ name, id, departmentName, departmentId });
+        });
+      });
+  
+      return clientMap;
+    } catch (error) {
+      console.error(error);
+      return {};
+    }
+  }
+  
 
 export async function getAllData() {
     const { SPREAD_SHEET_ID, GOOGLE_API_KEY } = getVars();
     
     const kamokuRange = "経費科目!A3:A";
-    const projectRange = "案件!A2:A";
-    
+    const projectTypeRange = "事業タイプ!A2:A";
+    const projectRange = "事業タイプ!B2:B";
+
     const kamokuUrl = `https://sheets.googleapis.com/v4/spreadsheets/${SPREAD_SHEET_ID}/values/${kamokuRange}?key=${GOOGLE_API_KEY}`;
+    const projectTypeUrl = `https://sheets.googleapis.com/v4/spreadsheets/${SPREAD_SHEET_ID}/values/${projectTypeRange}?key=${GOOGLE_API_KEY}`;
     const projectUrl = `https://sheets.googleapis.com/v4/spreadsheets/${SPREAD_SHEET_ID}/values/${projectRange}?key=${GOOGLE_API_KEY}`;
     
     try {
-        const [kamokuResponse, projectResponse] = await Promise.all([useFetch(kamokuUrl), useFetch(projectUrl)]);
+        const [kamokuResponse,projectTypeResponse, projectResponse] = await Promise.all([useFetch(kamokuUrl),useFetch(projectTypeUrl), useFetch(projectUrl)]);
         
         return {
             kamokuData: kamokuResponse.data,
+            projectTypeData: projectTypeResponse.data,
             projectData: projectResponse.data,
         };
     } catch (error) {
@@ -92,3 +114,4 @@ export async function getAllData() {
         return { error };
     }
 }
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,106 +1,155 @@
-<script setup>
-import { getAllData } from '@/composables/useSheet';
-import { useDayjs } from '#dayjs' 
-import { ref, onMounted } from 'vue';
+<template>
+    <v-container>
+      <!-- 事業詳細のセレクトボックス -->
+      <v-form ref="form" v-model="valid" lazy-validation class="mt-6 ml-6" style="width: 50%;">
+        <h2 class="my-6">売上入力</h2>
+        <v-select v-model="selectedDetail" :items="details" label="事業詳細を選択" @update:modelValue="updateType" required></v-select>
+        <v-select v-model="seikyuType" :items="seikyuTypes" label="請求タイプ" required></v-select>
+        <v-text-field v-show="false" label="非表示の項目" v-model="selectedType"></v-text-field>        
+        <v-text-field label="請求日" v-model="seikyuDate" input type="date"></v-text-field>
+        <v-select v-model="selectedClient" :items="clientCandidates" label="取引先を選択" item-title="name" item-value="name" required></v-select>
+        <v-text-field label="売上金額" v-model="uriagekingaku" :rules="amountRules" prefix="￥" required></v-text-field>
+        <v-text-field v-model="kenmei" label="件名を入力" required></v-text-field>
+        <div v-if="seikyuType === '定期'">
+          <v-text-field label="請求開始日" v-model="kaishibi" input type="date"></v-text-field>
+          <v-text-field label="請求終了日" v-model="shuryobi" input type="date"></v-text-field>
+        </div>
+        <div v-else>
+          <v-text-field label="支払期限日" v-model="shiharaikigenbi" input type="date"></v-text-field>
+        </div>
+        <v-textarea label="備考" v-model="memo" required></v-textarea>
+        <v-btn color="primary" @click="submitForm">送信</v-btn>
 
-const dayjs = useDayjs();
-const valid = ref(true);
-const projectName = ref(null);
-const amountRules = [
+         <!-- 折りたたみ可能な詳細セクション -->
+        <!-- <div class="mt-3">
+          <h3 class="my-2">【備考欄の記載方法】</h3>
+          <v-expansion-panels>
+            <v-expansion-panel v-for="(section, index) in sections" :key="index">
+              <v-expansion-panel-title>{{ section.title }}</v-expansion-panel-title>
+              <v-expansion-panel-text>
+                <div v-html="section.content"></div>
+              </v-expansion-panel-text>
+            </v-expansion-panel>
+          </v-expansion-panels>
+        </div> -->
+    </v-form>
+    </v-container>
+  </template>
+  
+  <script setup>
+  import { ref, onMounted,watch } from "vue";
+  import { getProjectData } from "~/composables/useSheet";
+  import { getClientData } from "~/composables/useSheet";
+  import { useDayjs } from '#dayjs' 
+  
+  const dayjs = useDayjs();
+  const projectData = ref({});
+  const details = ref([]);
+  const selectedDetail = ref(null);
+  const selectedType = ref(null);
+  const seikyuType = ref(null);
+  const seikyuTypes = ref(['都度','定期']);
+  const seikyuDate = ref(null);
+  const selectedClient = ref(null); // 選択された取引先
+  const clientMap = ref({}); // 事業詳細ごとの取引先データ
+  const clientCandidates = ref([]); // 選択肢として表示する取引先
+  const uriagekingaku = ref(null);
+  const amountRules = [
     v => !!v || '金額は必須です',
     v => !isNaN(v) || '金額は数字で入力してください'
-];
-const amount = ref(null);
-const kamoku = ref(null);
-const date = ref(null);
-const comment = ref(null);
-const project = ref([]);
-const kamokuItems = ref([]);
-const result = ref([]);
+  ];
+  const kenmei = ref(null);
+  const kaishibi = ref(null);
+  const shuryobi = ref(null);
+  const shiharaikigenbi = ref(null);
+  const memo = ref(null);
+  const parsedDate = dayjs(seikyuDate.value);
 
-(async () => {
-    await fetchData(); // 初期データを取得
-})();
-
-onMounted(async () => {
-    await fetchData();
-    initializeLiff('2005378903-nPYJQ5Bq');
-});
-
-async function fetchData() {
+  onMounted(async () => {
     try {
-        const { kamokuData, projectData, error } = await getAllData();
-        if (error) {
-            console.error('Error fetching data:', error);
-            return;
-        }
-
-        if (kamokuData && kamokuData.value && kamokuData.value.values) {
-            kamokuItems.value = kamokuData.value.values.map(item => item[0]);
-        } else {
-            console.error('Failed to load kamoku data');
-        }
-
-        if (projectData && projectData.value && projectData.value.values) {
-            project.value = projectData.value.values.map(item => item[0]);
-        } else {
-            console.error('Failed to load project data');
-        }
+      const data = await getProjectData();
+      projectData.value = data;
+  
+      // 事業詳細の一覧を取得
+      details.value = Object.keys(data);//Object.values(data).flat();
+      console.log(details.value);
     } catch (error) {
-        console.error('Error fetching data:', error);
+      console.error("データ取得エラー:", error);
     }
-}
 
-function submitForm() {
-    const message = `expenses\n${projectName.value}\n${kamoku.value}\n${amount.value}\n${dayjs(date.value).format('YYYY/MM/DD')}\n${comment.value}`;
-    sendMessages(message);
-}
+    try {
+      console.log("取引先データ取得");
+      clientMap.value = await getClientData();
+      console.log(clientMap.value);
 
-function initializeApp(){
-    liff.getProfile().then(profile => {
-        console.log('User profile:', profile);
-    }).catch((err) => {
-        console.log('Failed to get profile', err);
-    });
+      // 事業詳細の一覧を取得
+      details.value = Object.keys(clientMap.value);
+    } catch (error) {
+      console.error("データ取得エラー:", error);
+    }
+    
+    // 初期の請求日を基に関連フィールドを計算
+    seikyuDate.value = dayjs().format('YYYY-MM-DD');
+    updateDateFields(seikyuDate.value);
+    
+  });
 
-}
+  // 日付フィールドを更新する関数
+  const updateDateFields = (date) => {
+    const parsedDate = dayjs(date);
+    kaishibi.value = parsedDate.startOf("month").format("YYYY-MM-DD");
+    shuryobi.value = parsedDate.add(1, "month").endOf("month").format("YYYY-MM-DD");
+    shiharaikigenbi.value = parsedDate.add(1, "month").endOf("month").format("YYYY-MM-DD");
 
-function initializeLiff(liffId) {
-    const liffSDKUrl = 'https://static.line-scdn.net/liff/edge/2/sdk.js';
-    const script = document.createElement('script');
-    script.src = liffSDKUrl;
-    script.onload = () => {
-        liff.init({ liffId: liffId })
-        .then(() => {
-            initializeApp();
-        })
-        .catch((err) => {
-            console.log('LIFF Initialization failed ', err);
-        });
-    };
-    document.body.appendChild(script);
-}
+  };
+  
+  // 事業詳細の選択変更を監視
+  watch(selectedDetail, (newDetail) => {
+    console.log("選択された事業詳細:", newDetail);
 
-function sendMessages(message) {
-    liff.sendMessages([{
-        type: 'text',
-        text: message
-    }]).then(function() {
-        liff.closeWindow();
-    }).catch(function(error) {
-        console.error(error);
-    });
-}
-</script>
+    // 選択された事業詳細に基づいて事業タイプを更新
+    for (const [type, detailList] of Object.entries(projectData.value)) {
+      //if (detailList.includes(newDetail)) {
+      if (newDetail && projectData.value[newDetail]) {
+        const detailData = projectData.value[newDetail];
+        selectedType.value = detailData.type; // 事業タイプ
+        seikyuType.value = detailData.billingType; // 請求区分
+        kenmei.value = detailData.invoiceTitle; // 件名
+        memo.value = detailData.bikou; // 備考
+        console.log("更新された事業タイプ:", selectedType.value);
+        selectedType.value = type;
+        console.log("更新された事業タイプ:", selectedType.value);
+        if (newDetail && clientMap.value[newDetail]) {
+          console.log("該当する事業タイプが見つかりました");
+          clientCandidates.value = clientMap.value[newDetail];
+        } else {
+          clientCandidates.value = [];
+        }
+        return;
+      }
+    }
+    // 該当する事業詳細がない場合
+    selectedType.value = null;
+    console.log("該当する事業タイプが見つかりません");
+  });
 
-<template>
-  <v-form ref="form" v-model="valid" lazy-validation class="mt-6 ml-6" style="width: 50%;">
-    <h2 class="my-6">経費入力</h2>
-    <v-select v-model="projectName" label="案件" :items="project" required></v-select>
-    <v-select v-model="kamoku" label="勘定科目" :items="kamokuItems" required></v-select>
-    <v-text-field v-model="amount" label="金額"  prefix="￥" :rules="amountRules" @input="formatAmount"></v-text-field>
-    <v-text-field v-model="date" label="日付" type="date"></v-text-field>
-    <v-text-field v-model="comment" label="メモ"></v-text-field>
-    <v-btn color="primary" @click="submitForm">送信</v-btn>
-  </v-form>
-</template>
+  // 請求日の変更を監視
+  watch(seikyuDate, (newSeikyuDate) => {
+    console.log("請求日が変更されました:", newSeikyuDate);
+    if (newSeikyuDate) {
+      updateDateFields(newSeikyuDate);
+    }
+  });
+
+  // フォーム送信
+  const submitForm = () => {
+    if(seikyuType === '定期'){
+      const message = `earnings\n${selectedDetail.value}\n${seikyuType.value}\n${seikyuDate.value}\n${selectedClient.value}\n${uriagekingaku.value}\n${kenmei.value}\n${kaishibi.value}\n${shuryobi.value}\n\n${memo.value}`;
+      console.log(message);
+    }else{
+      const message = `earnings\n${selectedDetail.value}\n${seikyuType.value}\n${seikyuDate.value}\n${selectedClient.value}\n${uriagekingaku.value}\n${kenmei.value}\n\n\n${shiharaikigenbi.value}\n${memo.value}`;
+      console.log(message);
+    }
+  };
+  </script>
+  
